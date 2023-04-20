085521fd681fb47c015b33ed74edd928e0d0bcdd
pablopiquin <pablopiquin81@gmail.com>
2023-04-20 22:59:40 +0200

    Imagenes puestas en la pantalla Inicio

--- FUTKICKER/src/Controladores/FXML_VentanaInicioController.java
+++ FUTKICKER/src/Controladores/FXML_VentanaInicioController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -29,13 +30,19 @@ public class FXML_VentanaInicioController implements Initializable {
     private Button botonCargar;
     @FXML
     private Button botonSalir;
+    @FXML
+    private ImageView Logo;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Image fondo= new Image("/Imagenes/FONDO.gif");
+        Image logo= new Image("/Imagenes/Logo.png");
+        fondoImagen.setImage(fondo);
+        Logo.setImage(logo);
+        
     }    
 
     @FXML
