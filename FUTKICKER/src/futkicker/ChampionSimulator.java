57b7b0f749b05d569b4ab88b21c936bd8345330e
Matmart13 <mmmancini13@gmail.com>
2023-04-20 23:11:15 +0200

    Titulo y ImagenApp

--- FUTKICKER/src/futkicker/ChampionSimulator.java
+++ FUTKICKER/src/futkicker/ChampionSimulator.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -24,6 +25,8 @@ public class ChampionSimulator extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/Imagenes/LogoAPP.png"));
+        stage.setTitle("ChampionSimulator");
         stage.show();
     }
 
