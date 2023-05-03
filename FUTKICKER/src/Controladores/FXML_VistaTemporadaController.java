04189f2f9261f86a6f0689258c671e06bfe11edd
Matmart13 <mmmancini13@gmail.com>
2023-05-03 15:39:51 +0200

    Variable Para pasar pantalla y (error) boton RM
    He intentado hacer el cambio con el RM pero me da error. Lo subo para que lo veas tu tmbn a ver si lo ves.

--- FUTKICKER/src/Controladores/FXML_VistaTemporadaController.java
+++ FUTKICKER/src/Controladores/FXML_VistaTemporadaController.java
@@ -7,7 +7,9 @@ package Controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,13 +17,25 @@ import javafx.fxml.Initializable;
  * @author marti
  */
 public class FXML_VistaTemporadaController implements Initializable {
-
+    String nombre;
+    @FXML
+    private ImageView Logo;
+    @FXML
+    private ImageView EscudoEquipo;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
+
+    public FXML_VistaTemporadaController(String _nombre) {
+        this.nombre = _nombre;
+    }
+    
+    
+    
     
 }
