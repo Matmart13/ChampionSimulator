4c48f0bda963a38c08130469d2e49c1067fe6ef5
pablopiquin <pablopiquin81@gmail.com>
2023-05-09 17:06:21 +0200

    Estetica ventana partido y temporada

--- FUTKICKER/src/Controladores/FXML_VentanaPartidoController.java
+++ FUTKICKER/src/Controladores/FXML_VentanaPartidoController.java
@@ -7,7 +7,12 @@ package Controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -16,12 +21,34 @@ import javafx.fxml.Initializable;
  */
 public class FXML_VentanaPartidoController implements Initializable {
 
+    @FXML
+    private ImageView vs;
+    @FXML
+    private ImageView fondoPartido;
+    @FXML
+    private ImageView equipo1;
+    @FXML
+    private TextArea textField;
+    @FXML
+    private Label marcadorEq2;
+    @FXML
+    private ImageView equipo2;
+    @FXML
+    private Label marcadorEq1;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        Image vsFoto= new Image("/Imagenes/v1.png");
+        Image fondo= new Image("/Imagenes/FondoPartido.png");
+        
+        
+        
+        vs.setImage(vsFoto);
+        fondoPartido.setImage(fondo);
     }
 
 }
