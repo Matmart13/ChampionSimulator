f573fbef676f686b4163fcae274d41811c81207f
Matmart13 <mmmancini13@gmail.com>
2023-05-10 22:24:43 +0200

    mejora tiempo

--- FUTKICKER/src/Utiles/HiloTiempo.java
+++ FUTKICKER/src/Utiles/HiloTiempo.java
@@ -5,11 +5,13 @@
  */
 package Utiles;
 
+import java.util.Optional;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.util.Duration;
 
 /**
@@ -23,6 +25,7 @@ public class HiloTiempo extends Thread {
     Timeline timeline;
     private int max = 45;
     private int countParte = 0;
+     Image icono = new Image("/Imagenes/LogoAPP.png", 200, 100, false, true);
     public HiloTiempo(Label _label) {
         // TODO Auto-generated constructor stub
         label = _label;
@@ -34,19 +37,26 @@ public class HiloTiempo extends Thread {
         timeline = new Timeline(new KeyFrame(Duration.seconds(0.15), event -> {
             count++;
             label.setText(Integer.toString(count));
+            
             if (label.getText().equals(String.valueOf(max)) == true) {
                 timeline.stop();
-                Alert a = new Alert(Alert.AlertType.NONE);
+                Alert a = new Alert(Alert.AlertType.CONFIRMATION);
                 a.show();
-                if(countParte == 1){
-                    a.setTitle("Fin Partido");
-                    a.setContentText("Finalización partido");
-                }
+               
                 a.setTitle("Fin primera parte");
+                a.setHeaderText("Fin primera parte");
+                ButtonType botonSeguir = new ButtonType("SEGUIR");
+                
                 countParte = countParte+1;
-                a.setContentText("Finalización primera parte");
-                a.setResult(ButtonType.NEXT);
+                a.getButtonTypes().setAll(botonSeguir);
+                Optional<ButtonType> result = a.showAndWait() ;
+                if(result.get() == botonSeguir ){
+                     label.setText("0");
+                     
+                }
+
             }
+              
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
